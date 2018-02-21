c02b05e935ed92f6d0bd6f822b34068becfd04d1
djkeops <djkeopscris@yahoo.com>
2018-02-21 23:34:37 +0200

    Some improvements in code

--- app/src/main/java/com/example/android/footballscoreskeeper/MainActivity.java
+++ app/src/main/java/com/example/android/footballscoreskeeper/MainActivity.java
@@ -9,30 +9,21 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * Declaring TextViews.
-     * TextView home_team_score_view = (TextView) findViewById(R.id.home_team_score);
-     * TextView away_team_score_view = (TextView) findViewById(R.id.away_team_score);
-     * TextView home_team_r_cards_view = (TextView) findViewById(R.id.home_team_r_cards);
-     * TextView away_team_r_cards_view = (TextView) findViewById(R.id.away_team_r_cards);
-     * TextView home_team_y_cards_view = (TextView) findViewById(R.id.home_team_y_cards);
-     * TextView away_team_y_cards_view = (TextView) findViewById(R.id.away_team_y_cards);
-     */
-
-
+    //Initializing the scores.
     int scoreHomeTeam = 0;
     int scoreAwayTeam = 0;
     int rCardsHomeTeam = 0;
     int rCardsAwayTeam = 0;
     int yCardsHomeTeam = 0;
     int yCardsAwayTeam = 0;
+
+    //Declaring TextViews.
     TextView homeTeamScoreTextView;
     TextView awayTeamScoreTextView;
     TextView homeTeamRCardsTextView;
     TextView homeTeamYCardsTextView;
     TextView awayTeamRCardsTextView;
     TextView awayTeamYCardsTextView;
-
 
     Activity activity;
 
@@ -54,9 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         activity = MainActivity.this;
 
-        /**
-         * Define TextViews
-         */
+        //Defining TextViews
         homeTeamScoreTextView = (TextView) findViewById(R.id.home_team_score);
         awayTeamScoreTextView = (TextView) findViewById(R.id.away_team_score);
         homeTeamRCardsTextView = (TextView) findViewById(R.id.home_team_r_cards);
@@ -64,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         awayTeamRCardsTextView = (TextView) findViewById(R.id.away_team_r_cards);
         awayTeamYCardsTextView = (TextView) findViewById(R.id.away_team_y_cards);
 
+        //Getting the values from TextView if are saved
         if (savedInstanceState != null) {
             scoreHomeTeam = savedInstanceState.getInt("scoreHomeTeam");
             scoreAwayTeam = savedInstanceState.getInt("scoreAwayTeam");
@@ -81,36 +71,71 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Incrementing and displaying the Score for Home Team
+     *
+     * @param view home_team_score
+     */
     public void addScoreForHomeTeam(View view) {
         scoreHomeTeam = scoreHomeTeam + 1;
         displayScoreHomeTeam(scoreHomeTeam);
     }
 
+    /**
+     * Incrementing and displaying the Score for Away Team
+     *
+     * @param view away_team_score
+     */
     public void addScoreForAwayTeam(View view) {
         scoreAwayTeam = scoreAwayTeam + 1;
         displayScoreAwayTeam(scoreAwayTeam);
     }
 
+    /**
+     * Incrementing and displaying the Red Cards for Home Team
+     *
+     * @param view home_team_r_cards
+     */
     public void addRCardsForHomeTeam(View view) {
         rCardsHomeTeam = rCardsHomeTeam + 1;
         displayRCardsHomeTeam(rCardsHomeTeam);
     }
 
+    /**
+     * Incrementing and displaying the Red Cards for Away Team
+     *
+     * @param view away_team_r_cards
+     */
     public void addRCardsForAwayTeam(View view) {
         rCardsAwayTeam = rCardsAwayTeam + 1;
         displayRCardsAwayTeam(rCardsAwayTeam);
     }
 
+    /**
+     * Incrementing and displaying the Yellow Cards for Home Team
+     *
+     * @param view home_team_y_cards
+     */
     public void addYCardsForHomeTeam(View view) {
         yCardsHomeTeam = yCardsHomeTeam + 1;
         displayYCardsHomeTeam(yCardsHomeTeam);
     }
 
+    /**
+     * Incrementing and displaying the Yellow Cards for Away Team
+     *
+     * @param view away_team_y_cards
+     */
     public void addYCardsForAwayTeam(View view) {
         yCardsAwayTeam = yCardsAwayTeam + 1;
         displayYCardsAwayTeam(yCardsAwayTeam);
     }
 
+    /**
+     * Resetting all scores
+     *
+     * @param view all Scores TextView
+     */
     public void resetView(View view) {
         scoreHomeTeam = 0;
         scoreAwayTeam = 0;
@@ -128,7 +153,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Displays the given score for Home Team.
+     * Displaying the score for Home Team and (only in Portrait mode) changing the font size according to the numbers of the characters
+     *
+     * @param score the new score
      */
     public void displayScoreHomeTeam(int score) {
         homeTeamScoreTextView.setText(String.valueOf(score));
@@ -145,7 +172,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Displays the given score for Away Team.
+     * Displaying the score for Away Team and (only in Portrait mode) changing the font size according to the numbers of the characters
+     *
+     * @param score the new score
      */
     public void displayScoreAwayTeam(int score) {
         awayTeamScoreTextView.setText(String.valueOf(score));
@@ -162,28 +191,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Displays the red cards for Home Team.
+     * Displaying the Red Cards for Home Team.
      */
     public void displayRCardsHomeTeam(int score) {
         homeTeamRCardsTextView.setText(String.valueOf(score));
     }
 
     /**
-     * Displays the red cards for Away Team.
+     * Displaying the Red Cards for Away Team.
      */
     public void displayRCardsAwayTeam(int score) {
         awayTeamRCardsTextView.setText(String.valueOf(score));
     }
 
     /**
-     * Displays the yellow cards for Home Team.
+     * Displaying the Yellow Cards for Home Team.
      */
     public void displayYCardsHomeTeam(int score) {
         homeTeamYCardsTextView.setText(String.valueOf(score));
     }
 
     /**
-     * Displays the red cards for Away Team.
+     * Displaying the Red Cards for Away Team.
      */
     public void displayYCardsAwayTeam(int score) {
         awayTeamYCardsTextView.setText(String.valueOf(score));
